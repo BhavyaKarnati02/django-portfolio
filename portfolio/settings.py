e00df7690ff920f62da09f3b45b5bd449339feb2
Karnati <karnatibhavya1107@gmail.com>
2026-05-24 15:52:23 +0530

    Added STATIC_ROOT

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-fukarynbc$4wb*2v6e441q%u8x)8*p9do(&w6f8vnd$li@16zx
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS =  ['karnatibhavya.pythonanywhere.com']
 
 
 # Application definition
@@ -116,4 +116,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+STATIC_ROOT = '/home/KarnatiBhavya/staticfiles'
